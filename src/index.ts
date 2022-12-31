794ea12887d6ba955644662350eeab7862b7fb88
Andrei <andrei@laye.red>
2022-12-31 18:01:41 +0200

    updated: remove more params, handle more protocols

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-const { parse } = require('tldts')
+const { getDomain, parse } = require('tldts')
+
+interface SanitizeUrlOptions {
+	returns?: 'URL' | 'string';
+	allowedProtocols?: string[];
+}
 
 const hostnameRewrites: {[key: string]: string} = {
 	'instagram.com': 'www.instagram.com',
@@ -8,63 +13,84 @@ const hostnameRewrites: {[key: string]: string} = {
 	'mobile.twitter.com': 'twitter.com',
 }
 
-const secureDomains = ['google.com', 'twitter.com', 'facebook.com', 'instagram.com', 'tiktok.com', 'youtube.com', 'linkedin.com', 'whatsapp.com', 'tumblr.com']
+const secureDomains: string[] = ['google.com', 'twitter.com', 'facebook.com', 'instagram.com', 'tiktok.com', 'youtube.com', 'linkedin.com', 'whatsapp.com', 'tumblr.com']
 //todo use HSTS https://www.chromium.org/hsts/
 //https://raw.githubusercontent.com/chromium/chromium/main/net/http/transport_security_state_static.json
 
-const sanitizeUrl = (url: URL | string): string => {
+const trackingParams = ['utm_campaign', 'utm_content', 'utm_medium', 'utm_source', 'fbclid', 'ref', 'ref_src', 'ref_url', 'referer', 'ref_', 'fref', 'usp', 'trk', 'originalSubdomain', 'original_referer', 'share_app_id', 'share_author_id', 'share_link_id']
+const languageParams = ['locale', 'language', 'lang', 'Lang', 'hl']
+const domainParams: { [key: string]: string[] } = {
+	'instagram.com': ['igshid'],
+	'twitter.com': ['s', 't'],
+	'tiktok.com': ['_d', '_r', '_t', 'checksum', 'sec_uid', 'sec_user_id', 'tt_from', 'u_code', 'user_id'],
+}
+
+const ensureURL = (url: URL | string): URL => {
 	if (typeof url === 'string') {
-		url = new URL(url)
+		try {
+			url = new URL(url)
+		} catch (error) {
+			if (getDomain(url)) {
+				url = new URL(`http://${url}`)
+			} else {
+				throw new Error('Invalid URL')
+			}
+		}
+	}
+
+	return url
+}
+
+const sanitizeUrl = (url: URL | string, options?: SanitizeUrlOptions): string => {
+	const opts = {
+		returns: 'string',
+		allowedProtocols: ['http:', 'https:'],
+		...options,
+	}
+	const toURL = ensureURL(url)
+
+	if (!opts.allowedProtocols.includes(toURL.protocol)) {
+		throw new Error(`Invalid URL protocol "${toURL.protocol}"`)
 	}
 
 	// validate domain
-	const parsedDomain = parse(url.hostname)
+	const parsedDomain = parse(toURL.hostname)
 
 	if (!parsedDomain.domain || !parsedDomain.isIcann) {
-		throw new Error(`Not a valid internet domain "${url.hostname}"`)
+		throw new Error(`Not a valid internet domain "${toURL.hostname}"`)
 	}
 
 	// update hostname typos
-	if (hostnameRewrites[url.hostname]) {
-		url.hostname = hostnameRewrites[url.hostname]
+	if (hostnameRewrites[toURL.hostname]) {
+		toURL.hostname = hostnameRewrites[toURL.hostname]
 	}
 
 	// remove common prefixes (www., mobile.) for easier parsing
-	let noWwwHostname = url.hostname.replace(/^(www\.)/, '').replace(/^(mobile\.)/, '').replace(/^(m\.)/, '')
+	let noWwwHostname = toURL.hostname.replace(/^(www\.)/, '').replace(/^(mobile\.)/, '').replace(/^(m\.)/, '')
 
 	// upgrade domain protocol
-	if (secureDomains.includes(noWwwHostname) && url.protocol === 'http:') {
-		url.protocol = 'https:'
+	if (toURL.protocol === 'http:' && secureDomains.includes(noWwwHostname)) {
+		toURL.protocol = 'https:'
 	}
 
 	// remove tracking params
-	url.searchParams.delete('utm_campaign')
-	url.searchParams.delete('utm_content')
-	url.searchParams.delete('utm_medium')
-	url.searchParams.delete('utm_source')
-	url.searchParams.delete('fbclid')
-	url.searchParams.delete('igshid')
-	url.searchParams.delete('ref')
-	url.searchParams.delete('ref_')
-	url.searchParams.delete('fref')
-	url.searchParams.delete('usp')
-	url.searchParams.delete('trk')
-	url.searchParams.delete('originalSubdomain')
-	url.searchParams.delete('original_referer')
+	trackingParams.forEach(param => {
+		toURL.searchParams.delete(param)
+	})
 
 	// remove language params
-	url.searchParams.delete('locale')
-	url.searchParams.delete('lang')
-	url.searchParams.delete('Lang')
-	url.searchParams.delete('hl')
+	languageParams.forEach(param => {
+		toURL.searchParams.delete(param)
+	})
 
-	// remove Twitter tracking params
-	if (url.hostname === 'twitter.com') {
-		url.searchParams.delete('s')
-		url.searchParams.delete('t')
+	// remove domain-specific params
+	if (noWwwHostname in domainParams) {
+		domainParams[noWwwHostname].forEach(param => {
+			toURL.searchParams.delete(param)
+		})
 	}
 
-	return url.toString()
+	return toURL.toString()
 }
 
 const urlInfo = (url: URL | string): any => {
@@ -72,9 +98,18 @@ const urlInfo = (url: URL | string): any => {
 	const originalUrl = url
 	url = new URL(sanitized)
 
+	// validate domain
+	const parsedDomain = parse(url.hostname)
+
+	// remove common prefixes (www., mobile.) for easier parsing
+	let noWwwHostname = url.hostname.replace(/^(www\.)/, '').replace(/^(mobile\.)/, '').replace(/^(m\.)/, '')
+
 	return {
-		url: url.toString(),
+		originalUrl,
+		url: sanitized,
 		hostname: url.hostname,
+		domain: parsedDomain.domain,
+		subdomain: parsedDomain.subdomain,
 		group: '',
 		handle: null,
 		providerId: null,
--- tests/url-sanitize.spec.ts
+++ tests/url-sanitize.spec.ts
@@ -11,34 +11,63 @@ describe('sanitizeUrl()', function () {
 		assert.throws(() => sanitizeUrl('invalid-url'), Error);
 	});
 
+	it('invalid protocols', function () {
+		assert.throws(() => sanitizeUrl('ftp://ftp.host'), Error);
+		assert.throws(() => sanitizeUrl('site://site.com'), Error);
+		assert.throws(() => sanitizeUrl("javascript:alert('xss')"), Error);
+	});
+
+	it('allow ftp protocol', function () {
+		assert.equal('ftp://ftp.host/', sanitizeUrl('ftp://ftp.host', { allowedProtocols: ['http:', 'https:', 'ftp:'] }));
+	});
+
 	it('invalid tld', function () {
 		assert.throws(() => sanitizeUrl('http://domain.invalidtld'), Error);
 	});
 
+	it('invalid domain', function () {
+		assert.throws(() => sanitizeUrl('http://_-_.com'), Error);
+	});
+
+	it('ok example site, https', function () {
+		assert.equal('https://example.com/', sanitizeUrl('https://example.com'));
+	});
+
+	it('ok example site, http', function () {
+		assert.equal('http://example.com/', sanitizeUrl('http://example.com'));
+	});
+
+	it('no protocol, append http://', function () {
+		assert.equal('https://google.com/', sanitizeUrl('google.com'));
+		assert.equal('http://www.example.com/', sanitizeUrl('www.example.com'));
+	});
+
 	it('upgrade protocol to https on known secure domains', function () {
 		assert.equal('https://www.facebook.com/', sanitizeUrl('http://facebook.com'));
+		assert.equal('https://google.com/', sanitizeUrl('http://google.com'));
 	});
 
 	it('remove analyticcs/campaigns tracking params', function () {
 		assert.equal('https://www.facebook.com/oculus/', sanitizeUrl('https://www.facebook.com/oculus/?ref=bookmarks'));
 	});
 
-	it('remove IG/FB tracking params', function () {
-		const instagramShareUrl = 'https://instagram.com/instagram?igshid=YmMyMTA2M2Y='
-		const instagramShareUrlSanitized = 'https://www.instagram.com/instagram/'
+	it('remove language params', function () {
+		assert.equal('https://www.instagram.com/twitter/', sanitizeUrl('https://www.instagram.com/twitter/?hl=en'));
+	});
 
-		assert.equal(instagramShareUrlSanitized, sanitizeUrl(instagramShareUrl));
+	it('remove Meta (Instagram/Facebook/WhatsApp) tracking params', function () {
+		assert.equal('https://www.instagram.com/instagram', sanitizeUrl('https://instagram.com/instagram?igshid=YmMyMTA2M2Y='));
+		assert.equal('https://trailrunningacademy.com/', sanitizeUrl('https://trailrunningacademy.com/?fbclid=PAAabHWuo5mUnQXPoQ2CdbUdN_S8nJGA11yv8sdxRb5HfSphG_QYAv1f76MSQ'));
 	});
 
 	it('remove Twitter tracking params', function () {
-		const twitterShareUrl = 'https://twitter.com/Twitter/status/1601692766257709056?s=20&t=lBdttyHtwzrA27bJqu0g0Q'
-		const twitterShareUrlSanitized = 'https://twitter.com/Twitter/status/1601692766257709056'
-
-		assert.equal(twitterShareUrlSanitized, sanitizeUrl(twitterShareUrl));
+		assert.equal('https://twitter.com/Twitter/status/1601692766257709056', sanitizeUrl('https://twitter.com/Twitter/status/1601692766257709056?s=20&t=lBdttyHtwzrA27bJqu0g0Q'));
+		assert.equal('https://twitter.com/Twitter/status/1601692766257709056', sanitizeUrl('https://twitter.com/Twitter/status/1601692766257709056?ref_src=twsrc%5Etfw%7Ctwcamp%5Etweetembed%7Ctwterm%5E1598901113910333440%7Ctwgr%5E61781d6803d9d3ca6533629ca455faeab6f45cd2%7Ctwcon%5Es1_&ref_url=https%3A%2F%2Fdomaininvesting.com%2Fpage%2F2%2F'));
 	});
 
-	it('remove language params', function () {
-		assert.equal('https://www.instagram.com/twitter/', sanitizeUrl('https://www.instagram.com/twitter/?hl=en'));
+	it('remove TikTok tracking params', function () {
+		assert.equal('https://www.tiktok.com/@tiktok', sanitizeUrl('https://www.tiktok.com/@tiktok?_t=8WEXSJjCgJp&_r=1'));
+		assert.equal('https://www.tiktok.com/@tiktok', sanitizeUrl('https://www.tiktok.com/@tiktok?_d=secCgwIARCbDRjEFSADKAESPgo8dEq2Wni2E6WvJCanRSEzDDF0Qb3RpJoNdteKw%2FyQFEAOVm9Yeguo6SYmk56ER3KJdz%2FcPjxv3OhCauTjGgA%3D&checksum=6247ea4bffa090bbeafdcd95a1ee71f98b3dec4aa4480a8e4cad6b3479e8048b&language=he&sec_uid=MS4wLjABAAAAMVSsV5si5Q3TE-hoefIkSB4SfGiP7efmbW_sleWrFoQ4aNpdKNTVun1VeiEJvXWz&sec_user_id=MS4wLjABAAAAMVSsV5si5Q3TE-hoefIkSB4SfGiP7efmbW_sleWrFoQ4aNpdKNTVun1VeiEJvXWz&share_app_id=1233&share_author_id=6756995973303993349&share_link_id=BFA12B73-0A6C-4CAC-9A99-B7B5E513FAFF&tt_from=copy&u_code=d97f07a06fdd47&user_id=6756995973303993349&utm_campaign=client_share&utm_medium=ios&utm_source=copy&_r=1'));
 	});
 });
 
